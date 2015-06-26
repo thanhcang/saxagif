ac6e48bcc5ac88ebaa764b9c4bb257e4e3bdba31
thanhcang <vtcanglt@gmail.com>
2015-06-26 23:15:06 +0700

    Revert "delete file unuse"
    This reverts commit 8b036bf9caf0292ed0cf594c2ee079726c0bec72.

---
+++ dev_saxagif/08_Sourcecode/admin/application/controllers/login.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author vtcanglt@gmail.com
+ * @date 20152306
+ * Management Login
+ */
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Login extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+//        $this->load->model('mlogin');
+//        $this->lang->load('login');
+    }
+
+    public function index() {
+        echo 'abc'; exit;
+        
+    }
+
+    public function login() {
+        
+    }
+
+    public function checkValid() {
+        
+    }
+
+    public function logOut() {
+        
+    }
+
+}
