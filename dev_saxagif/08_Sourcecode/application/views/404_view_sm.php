5eaf506cdfba1e04210f6a3e26c30d995afb05e3
hnguyen0110 <hoahn@accjpn.com>
2015-08-09 18:28:40 +0700

    add page 404 desktop, mobile

--- dev_saxagif/08_Sourcecode/application/views/404_view_sm.php
+++ dev_saxagif/08_Sourcecode/application/views/404_view_sm.php
@@ -4,11 +4,11 @@
         <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link type="text/css" rel="stylesheet" href="<?php echo base_url('common/css/layout.css') ?>"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url('common/css/layout_sm.css') ?>"/>
     </head>
     <body style="position: relative">
-        <div class="error"><img src="<?php echo base_url('common/images/bg_error.png') ?>"/></div>
-        <div class="link_home"><a href="<?php echo base_url() ?>">trở về trang chủ</a></div>
+        <div class="error"><img src="<?php echo base_url('common/images/bg_error_sm.png') ?>"/></div>
+        <div class="link_home"><a href="<?php echo base_url() ?>">Trở về trang chủ</a></div>
         <script src="<?php echo base_url('common/js/jquery-1.8.1.min.js') ?>"></script>
         <script>
             function marLeft() {
